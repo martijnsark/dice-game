87a2851f22ca43a1004b35c779e6f9dcefce8858
martiijn <94526855+martijnsark@users.noreply.github.com>
2022-09-19 10:50:35 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(0, 10))
+    for (let value = 0; value <= 5; value++) {
+        if (true) {
+            basic.showString("you loose")
+            if (false) {
+                basic.showString("you win")
+            }
+        }
+    }
+})
